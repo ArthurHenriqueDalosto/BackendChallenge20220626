d3dc91dbce7c081b50c66673d2f8e571f43d509e
Arthur Henrique Dalosto <arthurdalosto@live.com>
2023-01-03 19:42:43 -0300

    appsettings.json bug resolved

--- ChallangeData/Migrations/20230103221151_v.0.1.Designer.cs
+++ ChallangeData/Migrations/20230103221151_v.0.1.Designer.cs
@@ -12,8 +12,8 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ChallangeData.Migrations
 {
     [DbContext(typeof(DataContext.DataContext))]
-    [Migration("20221230172407_settingNullables")]
-    partial class settingNullables
+    [Migration("20230103221151_v.0.1")]
+    partial class v01
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -25,45 +25,53 @@ namespace ChallangeData.Migrations
 
             NpgsqlModelBuilderExtensions.UseSerialColumns(modelBuilder);
 
-            modelBuilder.Entity("ChallangeData.Model.Product", b =>
+            modelBuilder.Entity("ChallangeData.Model.Product.Product", b =>
                 {
-                    b.Property<int?>("Id")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("integer");
 
-                    NpgsqlPropertyBuilderExtensions.UseSerialColumn(b.Property<int?>("Id"));
+                    NpgsqlPropertyBuilderExtensions.UseSerialColumn(b.Property<int>("Id"));
 
                     b.Property<string>("barcode")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("brands")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("categories")
+                        .IsRequired()
                         .HasColumnType("text");
 
-                    b.Property<int?>("code")
-                        .HasColumnType("integer");
+                    b.Property<long>("code")
+                        .HasColumnType("bigint");
 
                     b.Property<string>("image_url")
+                        .IsRequired()
                         .HasColumnType("text");
 
-                    b.Property<DateTime?>("imported_t")
+                    b.Property<DateTime>("imported_t")
                         .HasColumnType("timestamp with time zone");
 
                     b.Property<string>("packaging")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("product_name")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("quantity")
+                        .IsRequired()
                         .HasColumnType("text");
 
-                    b.Property<int?>("status")
+                    b.Property<int>("status")
                         .HasColumnType("integer");
 
                     b.Property<string>("url")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.HasKey("Id");
--- ChallangeData/Migrations/DataContextModelSnapshot.cs
+++ ChallangeData/Migrations/DataContextModelSnapshot.cs
@@ -22,45 +22,53 @@ namespace ChallangeData.Migrations
 
             NpgsqlModelBuilderExtensions.UseSerialColumns(modelBuilder);
 
-            modelBuilder.Entity("ChallangeData.Model.Product", b =>
+            modelBuilder.Entity("ChallangeData.Model.Product.Product", b =>
                 {
-                    b.Property<int?>("Id")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("integer");
 
-                    NpgsqlPropertyBuilderExtensions.UseSerialColumn(b.Property<int?>("Id"));
+                    NpgsqlPropertyBuilderExtensions.UseSerialColumn(b.Property<int>("Id"));
 
                     b.Property<string>("barcode")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("brands")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("categories")
+                        .IsRequired()
                         .HasColumnType("text");
 
-                    b.Property<long?>("code")
+                    b.Property<long>("code")
                         .HasColumnType("bigint");
 
                     b.Property<string>("image_url")
+                        .IsRequired()
                         .HasColumnType("text");
 
-                    b.Property<DateTime?>("imported_t")
+                    b.Property<DateTime>("imported_t")
                         .HasColumnType("timestamp with time zone");
 
                     b.Property<string>("packaging")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("product_name")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("quantity")
+                        .IsRequired()
                         .HasColumnType("text");
 
-                    b.Property<int?>("status")
+                    b.Property<int>("status")
                         .HasColumnType("integer");
 
                     b.Property<string>("url")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.HasKey("Id");
